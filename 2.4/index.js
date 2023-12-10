a47ad6ecfd3e76388ba96b8a993ce65b105bce7b
Nikita <ezin.nik@yandex.ru>
2023-12-10 10:24:43 +0700

    Переменные

---
+++ 2.4/index.js
@@ -0,0 +1,12 @@
+let admin;
+let name;
+
+name = "Джон";
+admin = name;
+alert(admin);
+
+let ourPlanetName = "Земля";
+let currentUserName = "Джон";
+
+const BIRTHDAY = "18.04.1982";
+const age = someCode(BIRTHDAY);
