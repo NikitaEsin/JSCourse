04f44ddd281c47f25a13ce1db3c34033991ea328
Nikita <ezin.nik@yandex.ru>
2023-12-17 15:45:45 +0700

    Промисы, async/await

---
+++ 11.2/index.js
@@ -0,0 +1,47 @@
+/*
+Встроенная функция setTimeout использует колбэк-функции. Создайте альтернативу, использующую промисы.
+Функция delay(ms) должна возвращать промис, который перейдёт в состояние «выполнен» через ms миллисекунд, так чтобы мы могли добавить к нему .then:
+
+function delay(ms) {
+  // ваш код
+}
+
+delay(3000).then(() => alert('выполнилось через 3 секунды')); */
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+delay(3000).then(() => alert('выполнилось через 3 секунды'));
+
+
+
+
+function go() {
+  showCircle(150, 150, 100).then(div => {
+    div.classList.add('message-ball');
+    div.append("Hello, world!");
+  });
+}
+
+function showCircle(cx, cy, radius) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'circle';
+  document.body.append(div);
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+
+      div.addEventListener('transitionend', function handler() {
+        div.removeEventListener('transitionend', handler);
+        resolve(div);
+      });
+    }, 0);
+  })
+}
