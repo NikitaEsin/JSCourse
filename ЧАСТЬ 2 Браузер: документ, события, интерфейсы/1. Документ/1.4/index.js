118628b07c703bbcfea73b724fb70e2c71fa5ae4
Nikita <ezin.nik@yandex.ru>
2023-12-22 14:05:27 +0700

    Поиск: getElement*, querySelector*

---
+++ ЧАСТЬ 2 Браузер: документ, события, интерфейсы/1. Документ/1.4/index.js
@@ -0,0 +1,42 @@
+/*
+Вот документ с таблицей и формой.
+Как найти?…
+Таблицу с id="age-table".
+Все элементы label внутри этой таблицы (их три).
+Первый td в этой таблице (со словом «Age»).
+Форму form с именем name="search".
+Первый input в этой форме.
+Последний input в этой форме. */
+
+// Есть много путей как это сделать.
+// Вот некоторые:
+
+// 1. Таблица с `id="age-table"`.
+let table = document.getElementById('age-table')
+
+// 2. Все label в этой таблице
+table.getElementsByTagName('label')
+// или
+document.querySelectorAll('#age-table label')
+
+// 3. Первый td в этой таблице
+table.rows[0].cells[0]
+// или
+table.getElementsByTagName('td')[0]
+// или
+table.querySelector('td')
+
+// 4. Форма с name="search"
+// предполагаем, что есть только один элемент с таким name в документе
+let form = document.getElementsByName('search')[0]
+// или, именно форма:
+document.querySelector('form[name="search"]')
+
+// 5. Первый input в этой форме
+form.getElementsByTagName('input')[0]
+// или
+form.querySelector('input')
+
+// 6. Последний input в этой форме
+let inputs = form.querySelectorAll('input') // найти все input
+inputs[inputs.length-1] // взять последний
