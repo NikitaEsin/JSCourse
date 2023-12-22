b13c727cd56281b77c131ba115720ac10c7a3761
Nikita <ezin.nik@yandex.ru>
2023-12-22 15:22:47 +0700

    Атрибуты и свойства

---
+++ ЧАСТЬ 2 Браузер: документ, события, интерфейсы/1. Документ/1.6/index.js
@@ -0,0 +1,52 @@
+// Напишите код для выбора элемента с атрибутом data-widget-name из документа и прочитайте его значение.
+/*
+<!DOCTYPE html>
+<html>
+<body>
+
+  <div data-widget-name="menu">Choose the genre</div>
+
+  <script>
+    // получаем элемент
+    let elem = document.querySelector('[data-widget-name]');
+
+    // читаем значение
+    alert(elem.dataset.widgetName);
+    // или так
+    alert(elem.getAttribute('data-widget-name'));
+  </script>
+</body>
+</html> */
+
+
+
+
+/*
+Сделайте все внешние ссылки оранжевыми, изменяя их свойство style.
+Ссылка является внешней, если:
+Её href содержит ://
+Но не начинается с http://internal.com.
+Пример: 
+
+<a name="list">the list</a>
+<ul>
+  <li><a href="http://google.com">http://google.com</a></li>
+  <li><a href="/tutorial">/tutorial.html</a></li>
+  <li><a href="local/path">local/path</a></li>
+  <li><a href="ftp://ftp.com/my.zip">ftp://ftp.com/my.zip</a></li>
+  <li><a href="http://nodejs.org">http://nodejs.org</a></li>
+  <li><a href="http://internal.com/test">http://internal.com/test</a></li>
+</ul>
+
+<script>
+  // добавление стиля для одной ссылки
+  let link = document.querySelector('a');
+  link.style.color = 'orange';
+</script> */
+
+// найти все ссылки, атрибут href у которых содержит ://
+// и при этом href не начинается с http://internal.com
+let selector = 'a[href*="://"]:not([href^="http://internal.com"])';
+let links = document.querySelectorAll(selector);
+
+links.forEach(link => link.style.color = 'orange');
